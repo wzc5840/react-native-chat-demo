ed8da31cd7e3f8da92583697e7b318f1171c6236
Symond Wang <wzc5840@gmail.com>
2020-05-28 10:35:32 +0900

    Temp commit

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import { Button, View, Text } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
+      <Text>Details Screen</Text>
     </View>
   );
 }
@@ -17,8 +29,17 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Overview1'}}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{title: 'Overview2'}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
